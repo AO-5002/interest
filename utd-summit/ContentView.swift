98cba1e3e5e8aba155ef78e7941896a6e8ec8b0f
AO-5002 <ao2005ortiz@gmail.com>
2026-01-29 11:33:34 -0600

    Initial Commit

---
+++ utd-summit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  utd-summit
+//
+//  Created by Andres Ortiz Osorio on 1/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ utd-summit/utd_summitApp.swift
@@ -0,0 +1,17 @@
+//
+//  utd_summitApp.swift
+//  utd-summit
+//
+//  Created by Andres Ortiz Osorio on 1/29/26.
+//
+
+import SwiftUI
+
+@main
+struct utd_summitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
