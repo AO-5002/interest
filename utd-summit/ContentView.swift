9d43666e2beece4e0c1363134fcff457c7f0d40f
AO-5002 <ao2005ortiz@gmail.com>
2026-02-04 16:51:23 -0600

    remove

--- utd-summit/ContentView.swift
+++ utd-summit/ContentView.swift
@@ -7,30 +7,15 @@
 
 import SwiftUI
 
-enum Tabs {
-    case Home, Feed, Profile, Search
-}
-
-
 struct ContentView: View {
-    
-    @State private var selectedTab: Tabs = .Home
-    
     var body: some View {
-        TabView(selection: $selectedTab) {
-            Tab("Home", systemImage: "house.fill", value: Tabs.Home){
-                HomeView()
-            }
-            Tab("Feed", systemImage: "heart.fill", value: Tabs.Feed){
-                FeedView()
-            }
-            Tab("Profile", systemImage: "person.fill", value: Tabs.Profile){
-                ProfileView()
-            }
-            Tab(value: Tabs.Search, role: .search) {
-                
-            }
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
         }
+        .padding()
     }
 }
 
--- utd-summit/utd_summitApp.swift
+++ utd-summit/utd_summitApp.swift
@@ -9,22 +9,9 @@ import SwiftUI
 
 @main
 struct utd_summitApp: App {
-    
-    @State private var authViewModel = AuthViewModel()
-    
     var body: some Scene {
         WindowGroup {
-            Group {
-                if authViewModel.isAuthenticated {
-                    ContentView()
-                }
-                else {
-                    AuthView(authViewModel: authViewModel)
-                }
-            }
-            .task {
-                await authViewModel.getInitialSession()
-            }
+            ContentView()
         }
     }
 }
